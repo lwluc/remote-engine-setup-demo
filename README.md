076cb998bc4fa225fb98e6289f579512ceddf3ec
Luc Weinbrecht <luc@weinbrecht.de>
2023-10-12 15:58:15 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Remote Engine Setup Demo
+
+This repo should provide a starting point how to apply the [remote engine pattern](https://camunda.com/blog/2022/02/moving-from-embedded-to-remote-workflow-engines/).
---
+++ worker/src/test/kotlin/de/luc/weinbrecht/bpm/worker/WorkerApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.luc.weinbrecht.bpm.worker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WorkerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
