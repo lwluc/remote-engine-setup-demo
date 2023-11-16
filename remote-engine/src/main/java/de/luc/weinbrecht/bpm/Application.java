beda8610c620a94a1e786f7bf8c51cef954dc38a
Luc Weinbrecht <luc@weinbrecht.de>
2023-11-16 21:10:26 +0100

    using liquibase for engine db setup and migration

--- remote-engine/src/main/java/de/luc/weinbrecht/bpm/Application.java
+++ remote-engine/src/main/java/de/luc/weinbrecht/bpm/Application.java
@@ -2,8 +2,10 @@ package de.luc.weinbrecht.bpm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.sql.init.dependency.DependsOnDatabaseInitialization;
 
 @SpringBootApplication
+@DependsOnDatabaseInitialization
 public class Application {
 
   public static void main(String... args) {
